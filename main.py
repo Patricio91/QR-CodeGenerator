281762f1247693bb1d02054e8621069dc2385415
Patricio Poncini <patricioponcini10@gmail.com>
2022-11-28 10:52:19 -0300

    Update changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import qrcode
 
-data = 'My first QR Code in Python'
+data = 'https://patricio-poncini-page.netlify.app/'
 qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
 
 qr.add_data(data)
 
 qr.make(fit = True)
-img = qr.make_image(fill_color = "grey", back_color = "white")
+img = qr.make_image(fill_color = "black", back_color = "white")
 
-img.save("C:/Users/Patricio/Desktop/proyectos/QR Generator/img/myqrcode2.png")
+img.save("C:/Users/Patricio/Desktop/proyectos/QR Generator/img/qrcode.png")
